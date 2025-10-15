af60a0dbd3de8eb23f9d2277437d25541958ac75
joaoplaguardia <joaopedrolaguardia@gmail.com>
2025-10-14 21:29:30 -0300

    added location service

---
+++ lib/core/services/location_service.dart
@@ -0,0 +1,27 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationService {
+  Future<Position> getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception('Serviço de localização desativado');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Permissão de localização negada');
+      }
+    }
+
+    return await Geolocator.getCurrentPosition(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.high,
+      ),
+    );
+  }
+}
