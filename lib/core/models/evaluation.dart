f1f8f8b38aa03d130d9cf94e0a9d1d59adec80c7
joaoplaguardia <joaopedrolaguardia@gmail.com>
2025-11-07 10:45:50 -0300

    getEvaluations added

--- lib/core/models/evaluation.dart
+++ lib/core/models/evaluation.dart
@@ -1,43 +1,82 @@
+import 'price.dart';
+import 'client.dart';
+import 'fuel.dart';
+import 'gas_station.dart';
+import 'package:flutter/foundation.dart';
+
 class Evaluation {
-  int id;
-  int priceId;
-  int trust;
-  DateTime date;
+  final int? id;
+  final Price price;
+  final Client client;
+  final bool trust;
+  final DateTime? date;
 
   Evaluation({
-    required this.id,
-    required this.priceId,
+    this.id,
+    required this.price,
     required this.trust,
-    required this.date,
+    required this.client,
+    this.date,
   });
 
   factory Evaluation.fromJson(Map<String, dynamic> json) {
+    debugPrint('Evaluation.fromJson received: $json');
+
+    // Handle price
+    final priceData = json['price'];
+    final Price parsedPrice = priceData != null
+        ? Price.fromJson(priceData)
+        : Price(
+      id: null,
+      price: 0.0,
+      fuel: Fuel(
+        id: null,
+        name: 'Combustível Desconhecido',
+        gasStation: GasStation(
+          id: null,
+          name: 'Posto Desconhecido',
+          latitude: 0.0,
+          longitude: 0.0,
+        ),
+      ),
+      client: Client(
+        id: null,
+        email: 'Email Desconhecido',
+        name: 'Cliente Desconhecido',
+        password: '',
+      ),
+    );
+
+    // Handle client
+    final clientData = json['client'];
+    final Client parsedClient = clientData != null
+        ? Client.fromJson(clientData)
+        : Client(
+      id: null,
+      email: 'Email Desconhecido',
+      name: 'Cliente Desconhecido',
+      password: '',
+    );
+
     return Evaluation(
       id: json['id'] != null
           ? (json['id'] is int
-              ? json['id']
-              : int.tryParse(json['id'].toString()) ?? 0)
-          : 0,
-      priceId: json['priceId'] != null
-          ? (json['priceId'] is int
-              ? json['priceId']
-              : int.tryParse(json['priceId'].toString()) ?? 0)
-          : 0,
-      trust: json['trust'] != null
-          ? (json['trust'] is int
-              ? json['trust']
-              : int.tryParse(json['trust'].toString()) ?? 0)
-          : 0,
-      date: json['date'] != null ? DateTime.parse(json['date']) : DateTime.now(),
+          ? json['id']
+          : int.tryParse(json['id'].toString()))
+          : null,
+      price: parsedPrice,
+      trust: json['trust'] ?? false,
+      client: parsedClient,
+      date: json['date'] != null ? DateTime.parse(json['date']) : null,
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'priceId': priceId,
+      'price': {'id': price.id},
+      'client': {'id': client.id},
       'trust': trust,
-      'date': date.toIso8601String(),
     };
   }
 }
--- lib/core/services/evaluation_service.dart
+++ lib/core/services/evaluation_service.dart
@@ -4,23 +4,84 @@ import 'package:http/http.dart' as http;
 import '../config/api_config.dart';
 import '../models/evaluation.dart';
 import 'auth_helper.dart';
+import '../models/client.dart';
+import '../models/price.dart';
 
 class EvaluationService {
   final String baseUrl = '${ApiConfig.baseUrl}/evaluations';
 
-  Future<Evaluation> createEvaluation(Evaluation evaluation) async {
+  Future<void> createEvaluation({
+    required Client client,
+    required Price price,
+    required bool trust,
+}) async {
+    final body = {
+      'client': client.toJson(),
+      'price': price.toJson(),
+      'trust': trust,
+    };
+
     final response = await http.post(
       Uri.parse(baseUrl),
       headers: createAuthHeaders(),
-      body: jsonEncode(evaluation.toJson()),
+      body: jsonEncode(body),
     );
     debugPrint('Response status: ${response.statusCode}');
     debugPrint('Response body: ${response.body}');
 
-    if (response.statusCode == 201) {
-      return Evaluation.fromJson(jsonDecode(response.body));
-    } else {
+    if (response.statusCode != 201) {
       throw Exception('Falha ao criar avaliação');
     }
   }
+
+  Future<Map<String, Map<String, int>>> getEvaluationsbyGasStation(int id) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/trust-by-gasstation/$id'),
+      headers: createAuthHeaders(),
+    );
+
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body) as Map<String, dynamic>;
+
+      final Map<String, Map<String, int>> result = {};
+
+      decoded.forEach((fuelName, rawData) {
+        final data = rawData as Map<String, dynamic>? ?? {};
+        final likes = (data['likes'] as num?)?.toInt() ?? 0;
+        final dislikes = (data['dislikes'] as num?)?.toInt() ?? 0;
+        result[fuelName] = {'likes': likes, 'dislikes': dislikes};
+      });
+
+      return result;
+    } else {
+      debugPrint('Erro ao buscar avaliações: ${response.statusCode} ${response.body}');
+      throw Exception('Falha ao buscar avaliações');
+    }
+  }
+
+  Future<Map<String, Map<String, int>>> getEvaluationsbyPrice(int id) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/trust-by-price/$id'),
+      headers: createAuthHeaders(),
+    );
+
+    if (response.statusCode == 200) {
+      final decoded = jsonDecode(response.body) as Map<String, dynamic>;
+
+      final Map<String, Map<String, int>> result = {};
+
+      decoded.forEach((fuelName, rawData) {
+        final data = rawData as Map<String, dynamic>? ?? {};
+        final likes = (data['likes'] as num?)?.toInt() ?? 0;
+        final dislikes = (data['dislikes'] as num?)?.toInt() ?? 0;
+        result[fuelName] = {'likes': likes, 'dislikes': dislikes};
+      });
+
+      return result;
+    } else {
+      debugPrint('Erro ao buscar avaliações: ${response.statusCode} ${response.body}');
+      throw Exception('Falha ao buscar avaliações');
+    }
+  }
+
 }
